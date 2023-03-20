b0a597984f569f9f5cb5fa3d3a5fdfce7f23abad
Yakov Wildfluss <yakov@copra.de>
2023-03-20 18:39:56 +0300

    rename again

--- Formula/ffmpeg-gcc.rb
+++ Formula/ffmpeg-gcc.rb
@@ -1,4 +1,4 @@
-class FfmpegDebug < Formula
+class FfmpegGcc < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-6.0.tar.xz"
